fd0b22d4bbc7e4a596fb62cb6bd7165f9e518486
sunny90401sm <76708186+sunny90401sm@users.noreply.github.com>
2026-04-07 21:40:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
-const filesPath = path.join(__dirname, "html5up");
+const filesPath = path.join(__dirname, "test");
 
-app.use("/html5up", express.static(filesPath), serveIndex(filesPath, { icons: true }));
+app.use("/test", express.static(filesPath), serveIndex(filesPath, { icons: true }));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
